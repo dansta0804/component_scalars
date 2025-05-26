c013cc7d92198b06ceb299ba2eb39ebcd5e0964b
Danielė <97364205+dansta0804@users.noreply.github.com>
2025-05-26 20:24:51 +0300

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "vdot",
+    version = "0.1.0",
+    description = "A Python component that reads .dat files and calculates"
+                  "scalar products. If -f parameter is used, then the scalar"
+                  "product is displayed in specified format.",
+    author = "Danielė Stasiūnaitė",
+    author_email = "danielestasiunaite@gmail.com",
+    url = "https://github.com/dansta0804/component_scalars.git",
+    packages = find_packages(),
+    install_requires = [
+        # dependencies
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
